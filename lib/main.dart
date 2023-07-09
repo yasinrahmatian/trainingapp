eb70ef6c8838edf8c2450dcea075a0869c3d2a7c
yasinrahmatian <yasinrahmatian95@gmail.com>
2023-07-09 08:30:41 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:example1/pages/welcome_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: WelcomePage(),
+    );
+  }
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,93 @@
+import 'package:example1/widgets/app_large_text.dart';
+import 'package:example1/widgets/responsive_button.dart';
+import 'package:flutter/material.dart';
+
+import '../misc/colors.dart';
+import '../widgets/app_text.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (_, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'img/' + images[index],
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(
+                        text: "Mountain",
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ResponsiveButton()
+                    ],
+                  ),
+                  Column(
+                    children: List.generate(3, (indexDots) {
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 2),
+                        width: 8,
+                        height: index == indexDots ? 25 : 8,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexDots
+                                ? AppColors.mainColor
+                                : AppColors.mainColor.withOpacity(0.3)),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
